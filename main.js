c2c10ce4b2e3838bea51fc6b64d40022b6d18673
isabeledesouza <de.souza.isabele@escola.pr.gov.br>
2024-09-23 12:25:07 -0300

    f

--- main.js
+++ main.js
@@ -11,4 +11,20 @@ function areaquadrado(lado){
 console.log(areaquadrado(5));
 let areatriangulo= function(base, altura,divisao){
     return base * altura / divisao}
+
 console.log(areatriangulo(732,533,2))
+
+let areaTriangulo =
+function (base, altura) {
+    return (base*altura)/2;
+}
+console.log (areaTriangulo)
+
+let a = parseInt(prompt ("primeiro valor"))
+let b = parseInt(prompt ("segundo valor"))
+let c = parseInt(prompt ("terceiro valor"))
+
+function media(a,b,c) {
+    return((a+b+c)/3);
+}
+document.write(media(a, b, c))
