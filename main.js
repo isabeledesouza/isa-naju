fd1e1702a760fcde32f5a69de7b38d8c94fe99ee
isabeledesouza <de.souza.isabele@escola.pr.gov.br>
2024-09-23 11:52:06 -0300

    c

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ let nomedeusuario = "isa-naju"
 function soma (num1, num2) {
 return num1+num2
 }
+console.log(soma(5,5));
+
+function areaquadrado(lado){
+    return lado **2
+}
+console.log(areaquadrado(5));
+let areatriangulo= function(base, altura, 2)
+return base * altura
