43cd0e6621c71c2d7154b73208b4895829937a9f
isabeledesouza <de.souza.isabele@escola.pr.gov.br>
2024-09-30 11:29:43 -0300

    .

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-let nomedeusuario = "isa-naju"
-// alert ('nomeUsuario')
-function soma (num1, num2) {
-return num1+num2
-}
-console.log(soma(5,5));
+// let nomedeusuario = "isa-naju"
+// // alert ('nomeUsuario')
+// function soma (num1, num2) {
+// return num1+num2
+// }
+// console.log(soma(5,5));
 
-function areaquadrado(lado){
-    return lado **2
-}
-console.log(areaquadrado(5));
-let areatriangulo= function(base, altura,divisao){
-    return base * altura / divisao}
+// function areaquadrado(lado){
+//     return lado **2
+// }
+// console.log(areaquadrado(5));
+// let areatriangulo= function(base, altura,divisao){
+//     return base * altura / divisao}
 
-console.log(areatriangulo(732,533,2))
+// console.log(areatriangulo(732,533,2))
 
-let areaTriangulo =
-function (base, altura) {
-    return (base*altura)/2;
-}
-console.log (areaTriangulo)
+// let areaTriangulo =
+// function (base, altura) {
+//     return (base*altura)/2;
+// }
+// console.log (areaTriangulo)
 
-let a = parseInt(prompt ("primeiro valor"))
-let b = parseInt(prompt ("segundo valor"))
-let c = parseInt(prompt ("terceiro valor"))
+// let a = parseInt(prompt ("primeiro valor"))
+// let b = parseInt(prompt ("segundo valor"))
+// let c = parseInt(prompt ("terceiro valor"))
 
-function media(a,b,c) {
-    return((a+b+c)/3);
-}
-document.write(media(a, b, c))
+// function media(a,b,c) {
+//     return((a+b+c)/3);
+// }
+// document.write(media(a, b, c))
+let estados =['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
